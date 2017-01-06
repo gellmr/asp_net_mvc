d9d49d522f8cfdeb8a1674c20d9953d6d505fe42
gellmr <gellmr@gmail.com>
2017-01-06 11:52:47 +0800

    Added new MVC5 application from template. Added recommended .gitignore file

---
+++ asp_net_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_net_mvc.Startup))]
+namespace asp_net_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
