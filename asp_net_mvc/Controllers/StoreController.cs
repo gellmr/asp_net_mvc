f1b66a98505d174fcf8e0775fc548e9387096555
gellmr <gellmr@gmail.com>
2017-01-06 18:07:46 +0800

    Working on Store/index page. _ForSaleProductLine is half constructed.

--- asp_net_mvc/Controllers/StoreController.cs
+++ asp_net_mvc/Controllers/StoreController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using asp_net_mvc.ViewModels.Store;
 
 namespace asp_net_mvc.Controllers
 {
     public class StoreController : Controller
     {
-        // GET: Store
+        // GET: /Store/Index
         public ActionResult Index()
         {
-            return View();
+            IndexViewModel model = new IndexViewModel();
+            model.products = new List<ProductViewModel>();
+            model.products.Add(new ProductViewModel {
+                id = 1,
+                name = "0.01uF/10nF 3kV Ceramic Capacitor",
+                description = "High Voltage",
+                unitPrice = 2.5M,
+                imageUrl = "/images/120x120/3kVCeramicCapacitor.jpg",
+                quantityInStock = 140
+            });
+            return View(model);
         }
     }
 }
---
+++ asp_net_mvc/Models/StoreViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_net_mvc.ViewModels.Store
+{
+    public class IndexViewModel
+    {
+        public IList<ProductViewModel> products { get; set; }
+    }
+
+    // Product View Model to appear on the Store Index Page
+    public class ProductViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal unitPrice { get; set; }
+        public string imageUrl { get; set; }
+        public int quantityInStock { get; set; }
+    }
+}
